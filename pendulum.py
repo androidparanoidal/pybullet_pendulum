5ad71f963596b258ed7fb7f57a6ab50f2a882209
androidparanoidal <51123541+dogandroidparanoid@users.noreply.github.com>
2021-11-08 21:04:12 +0300

    hellomoto

--- pendulum.py
+++ pendulum.py
@@ -2,12 +2,16 @@ import pybullet as p
 import time
 import pybullet_data
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = 0
 dt = 1/240 # pybullet simulation step
 q0 = 0.5   # starting position (radian)
 physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
+#planeId = p.loadURDF("plane.urdf")
 boxId = p.loadURDF("./simple.urdf", useFixedBase=True)
 
 # get rid of all the default damping forces
@@ -21,7 +25,27 @@ for _ in range(1000):
 
 # turn off the motor for the free motion
 p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
-while True:
+
+time_list = []
+position_list = []
+
+
+
+#while True:
+#    p.stepSimulation()
+#    time.sleep(dt)
+
+while t < 5:
     p.stepSimulation()
     time.sleep(dt)
+
+    time_list.append(t)
+    t+=dt
+
+    jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
+    position_list.append(jointPosition)
+
+plt.plot(time_list, position_list)
+plt.show()
+
 p.disconnect()
