a83e5fb5bb8a6723ff49e694714f1d07a87a0550
androidparanoidal <51123541+dogandroidparanoid@users.noreply.github.com>
2021-11-11 00:55:27 +0300

    Added numerical method Euler's

--- pendulum.py
+++ pendulum.py
@@ -43,18 +43,37 @@ while t < 5:
     jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
     position_list.append(jointPosition)
 
+
+# Численное решение с помощью odeint
 length = 0.8
 g = 9.81
 koef = g/length
 q0_initial = [0.5, 0]  # нач знач: x(t=0) = 0.5, x'(t=0) = 0
 
-# d^x/dt^2 + g/l * x = 0
+# d^x/dt^2 + g/l * x = 0 решение которого: x(t) = c1*sin(sqrt(koef)*t) + c2*cos(sqrt(koef)*t)
 def diff_pend(x, t):
     return [x[1], -koef * x[0]]
 
-t = np.linspace(0, 5, 1200)
-solution = odeint(diff_pend, q0_initial, t)   # решение: x(t) = c1 * sin(sqrt(koef)*t) + c2 * cos(sqrt(koef)*t)
+t = np.linspace(0, 5, 5*240)
+solution = odeint(diff_pend, q0_initial, t)
 xs = solution[:, 0]
+
+
+# Численное решение по полунеявному м Эйлера (схема интегрирования в pybullet): v' = -koef*u и u'=v, замена в исходном v=x' и u=x
+u0 = 0.5
+v0 = 0
+n = 1200 # кол-во шагов
+h = 1/240 # шаг dt
+u = np.zeros(n+1)
+v = np.zeros(n+1)
+vremya = np.linspace(0, n*h, n+1)
+u[0] = u0
+v[0] = v0
+
+for i in range(n):
+    v[i+1] = v[i] - koef * h * u[i]
+    u[i+1] = u[i] + h * v[i+1]
+
 
 def L2_norm(xs, position_list):
     distance = np.sqrt(abs((np.array(xs) - np.array(position_list))**2))
@@ -67,11 +86,12 @@ plt.figure()
 plt.grid(True)
 plt.xlabel('t', fontsize=12)
 plt.ylabel('x(t)', fontsize=12)
-plt.plot(time_list, position_list, color='r', label='pybullet')
-plt.plot(t, xs, color='b', label='численное', linestyle = ':')
+plt.plot(time_list, position_list, color='c', label='симуляторное')
+plt.plot(t, xs, color='k', label='численное', linestyle = ':')
+plt.plot(vremya, u, color='r', label='эйлер')
 plt.legend(loc='best')
 plt.title('Сравнение симуляторного и численного решений:')
-plt.text(0, -0.4, "L2 Норма = ПОНЯТЬ КАК ДОБАВИТЬ ПЕРЕМЕННУЮ....... ",)
+#plt.text(0, -0.4, "L2 Норма = ПОНЯТЬ КАК ДОБАВИТЬ ПЕРЕМЕННУЮ....... ",)
 plt.show()
 
 p.disconnect()
