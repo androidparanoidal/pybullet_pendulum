cf12e15eddfdab6c2d4af1e5926a94b21dcb8886
androidparanoidal <51123541+dogandroidparanoid@users.noreply.github.com>
2021-11-16 15:54:17 +0300

    Arduino prog

---
+++ sketch.ino
@@ -0,0 +1,15 @@
+int sensorPin = A5;    // Порт к которому подключен потенциометр
+int sensorValue = 0;  // Переменная для хранения показаний
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  //Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print(",");
+  delay(25);
+}
