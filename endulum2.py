1a3d6f3497d474c247010d37ec25819b87a5139e
androidparanoidal <51123541+dogandroidparanoid@users.noreply.github.com>
2022-02-05 23:16:29 +0300

    Updated PI-controller

--- endulum2.py
+++ endulum2.py
@@ -20,35 +20,41 @@ dt = 1 / 240  # simulation step
 q0 = 0        # starting position
 q1_desirable = 1
 K_p = [0.5, 2, 4, 20]
+kp = 0.8
+ki = 3.3
 n = 4
 # position_list = np.zeros((n, 1200))
 position_list = []
 time_list = []
 int_error = 0
 
-# go to the starting position
-p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=q0, controlMode=p.POSITION_CONTROL)
-for _ in range(1000):
-    p.stepSimulation()
 
-jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
-q0 = jointPosition  # exact starting position
+def func(t, int_error, q1_desirable, q0, kp, ki):
+    # go to the starting position
+    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=q0, controlMode=p.POSITION_CONTROL)
+    for _ in range(1000):
+        p.stepSimulation()
 
-p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
+    jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
+    q0 = jointPosition  # exact starting position
 
-while t <= 10:
-    q1_current, *_ = p.getJointState(boxId, jointIndex=1)
-    position_list.append(q1_current)
-    error = q1_desirable - q1_current
-    int_error += error * dt
-    torque = K_p[0] * error + K_p[0] * int_error
-    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.TORQUE_CONTROL, force=torque)
-    p.stepSimulation()
-    time_list.append(t)
-    t += dt
-    #time.sleep(dt)
+    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
 
-''' 2 '''
+    while t <= 10:
+        q1_current, *_ = p.getJointState(boxId, jointIndex=1)
+        position_list.append(q1_current)
+        error = q1_desirable - q1_current
+        int_error += error * dt
+        torque = kp * error + ki * int_error
+        p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.TORQUE_CONTROL, force=torque)
+        p.stepSimulation()
+        time_list.append(t)
+        t += dt
+    return(position_list, time_list)
+
+sol = func(t, int_error, q1_desirable, q0, kp, ki)
+
+''' 2 
 t = 0
 int_error = 0
 
@@ -77,7 +83,7 @@ while t <= 10:
     #time.sleep(dt)
 
 
-''' 3 '''
+
 t = 0
 int_error = 0
 
@@ -104,43 +110,13 @@ while t <= 10:
     time_list3.append(t)
     t += dt
     #time.sleep(dt)
-
-
-''' 4 '''
-t = 0
-int_error = 0
-
-# go to the starting position 3
-p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=q0, controlMode=p.POSITION_CONTROL)
-for _ in range(1000):
-    p.stepSimulation()
-
-jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
-q0 = jointPosition  # exact starting position
-
-p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
-position_list4 = []
-time_list4 = []
-
-while t <= 10:
-    q1_current4, *_ = p.getJointState(boxId, jointIndex=1)
-    position_list4.append(q1_current4)
-    error = q1_desirable - q1_current4
-    int_error += error * dt
-    torque = K_p[3] * error + K_p[3] * int_error
-    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.TORQUE_CONTROL, force=torque)
-    p.stepSimulation()
-    time_list4.append(t)
-    t += dt
-    #time.sleep(dt)
+'''
 
 
 print(position_list)
 #print(np.size(position_list))
 print(time_list, '\n')
 
-print(position_list2)
-print(time_list2, '\n')
 
 t_q = np.linspace(0, 10)
 q_d = np.full(50, 1)
@@ -150,10 +126,9 @@ plt.grid(True)
 plt.title('Решения для желаемого q = {}:'.format(q1_desirable))
 plt.xlabel('t', fontsize=12)
 plt.ylabel('x(t)', fontsize=12)
-plt.plot(time_list, position_list, color='c', label="При K_p и К_i = 0.5")
-plt.plot(time_list, position_list2, color='r', label="При K_p и К_i = 2")
-plt.plot(time_list, position_list3, color='m', label="При K_p и К_i = 4")
-plt.plot(time_list, position_list4, color='g', label="При K_p и К_i = 20")
+#plt.plot(time_list, position_list2, color='r', label="При K_p и К_i = 2")
+#plt.plot(time_list, position_list3, color='m', label="При K_p и К_i = 4")
+plt.plot(time_list, position_list, color='c', label="При K_p = {} и К_i = {}".format(kp, ki), linewidth=2)
 plt.plot(t_q, q_d, color='k', label="Желаемое значение q", linewidth=2)
 plt.legend(loc='best')
 plt.show()
