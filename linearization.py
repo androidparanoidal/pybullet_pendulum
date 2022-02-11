7c435b13649c8ce27017bd62493fbd6dfd355787
androidparanoidal <51123541+dogandroidparanoid@users.noreply.github.com>
2022-02-11 22:06:55 +0300

    Update

--- linearization.py
+++ linearization.py
@@ -4,6 +4,7 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import pylab
 import numpy as np
+import math
 import time
 
 p.connect(p.GUI)
@@ -16,7 +17,7 @@ boxId = p.loadURDF("./simple.urdf", useFixedBase=True)
 p.changeDynamics(boxId, 1, linearDamping=0, angularDamping=0)
 p.changeDynamics(boxId, 2, linearDamping=0, angularDamping=0)
 
-# q0 = 0.1  # starting position (radian) 1.58
+# q0 = 0  # starting position (radian)
 time_list = []
 position_list = []
 
@@ -32,8 +33,7 @@ def sim_solution(q0):
     jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
     q0 = jointPosition
 
-    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
-
+    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0.1)
     position_list = []
     while t < 5:
         jointPosition, *_ = p.getJointState(boxId, jointIndex=1)
@@ -43,9 +43,9 @@ def sim_solution(q0):
         t += dt
     return position_list
 
-simsol1 = sim_solution(0.1)
-simsol2 = sim_solution(0.5)
-simsol3 = sim_solution(1)
+#simsol1 = sim_solution(0.5)
+#simsol2 = sim_solution((math.pi)/2)
+simsol3 = sim_solution(0)
 
 
 # Численное решение линейной системы d^2x/dt^2 +  b/(m*l*l) * dx/dt + g/l * x = 0
@@ -53,7 +53,7 @@ b = 1
 m = 1
 length = 0.8
 g = 9.81
-# q0_initial = [q0, 0]  # нач знач: x(t=0) = 0.1 = q0, x'(t=0) = 0
+# q0_initial = [q0, 0]  # нач знач: x(t=0) = 0 = q0, x'(t=0) = 0
 
 def odesol(X, t, b, m, length, g):
     x, x_new = X
@@ -62,9 +62,9 @@ def odesol(X, t, b, m, length, g):
 
 
 t = np.linspace(0, 5, 5*240)
-solution1 = odeint(odesol, [0.1, 0], t, args=(b, m, length, g))
-solution2 = odeint(odesol, [0.5, 0], t, args=(b, m, length, g))
-solution3 = odeint(odesol, [1, 0], t, args=(b, m, length, g))
+#solution1 = odeint(odesol, [0.5, 0], t, args=(b, m, length, g))
+#solution2 = odeint(odesol, [(math.pi)/2, 0], t, args=(b, m, length, g))
+solution3 = odeint(odesol, [0, 0], t, args=(b, m, length, g))
 
 
 pylab.figure(1)
@@ -72,38 +72,38 @@ pylab.grid()
 pylab.title("Сравнение симуляторного и численного решения линейной системы:")
 pylab.xlabel('t', fontsize=12)
 pylab.ylabel('x(t)', fontsize=12)
-pylab.plot(t, simsol1, color='m', label='Симуляторное при q0 = 0.1')
-pylab.plot(t, solution1[:, 0], color='k', label='Линейная система при q0 = 0.1', linestyle=':')
-pylab.plot(t, simsol2, color='g', label='Симуляторное при q0 = 0.5')
-pylab.plot(t, solution2[:, 0], color='k', label='Линейная система при q0 = 0.5', linestyle=':')
-pylab.plot(t, simsol3, color='c', label='Симуляторное при q0 = 1')
-pylab.plot(t, solution3[:, 0], color='k', label='Линейная система при q0 = 1', linestyle=':')
+#pylab.plot(t, simsol1, color='m', label='Симуляторное при q0 = 0.1')
+#pylab.plot(t, solution1[:, 0], color='k', label='Линейная система при q0 = 0.1', linestyle=':')
+#pylab.plot(t, simsol2, color='g', label='Симуляторное при q0 = pi/2')
+#pylab.plot(t, solution2[:, 0], color='k', label='Линейная система при q0 = pi/2', linestyle=':')
+pylab.plot(t, simsol3, color='c', label='Симуляторное при q0 = 0')
+pylab.plot(t, solution3[:, 0], color='k', label='Линейная система при q0 = 0', linestyle=':')
 pylab.legend()
 
-
+'''
 pylab.figure(2)  # наверное можно циклом нарисовать..........
 pylab.grid()
 pylab.subplot(2, 3, 1)
 pylab.xlabel('t', fontsize=12)
 pylab.ylabel('x(t)', fontsize=12)
-pylab.plot(t, simsol1, color='m', label='Симуляторное при q0 = 0.1')
-pylab.plot(t, solution1[:, 0], color='k', label='Линейная система при q0 = 0.1', linestyle=':')
+pylab.plot(t, simsol1, color='m', label='Симуляторное при q0 = 0.5')
+pylab.plot(t, solution1[:, 0], color='k', label='Линейная система при q0 = 0.5', linestyle=':')
 pylab.legend(loc='best')
 
 pylab.subplot(2, 3, 2)
 pylab.xlabel('t', fontsize=12)
 pylab.ylabel('x(t)', fontsize=12)
-pylab.plot(t, simsol2, color='m', label='Симуляторное при q0 = 0.5')
-pylab.plot(t, solution2[:, 0], color='k', label='Линейная система при q0 = 0.5', linestyle=':')
+pylab.plot(t, simsol2, color='m', label='Симуляторное при q0 = pi/2')
+pylab.plot(t, solution2[:, 0], color='k', label='Линейная система при q0 = pi/2', linestyle=':')
 pylab.legend(loc='best')
 
 pylab.subplot(2, 3, 3)
 pylab.xlabel('t', fontsize=12)
 pylab.ylabel('x(t)', fontsize=12)
-pylab.plot(t, simsol3, color='m', label='Симуляторное при q0 = 1')
-pylab.plot(t, solution3[:, 0], color='k', label='Линейная система при q0 = 1', linestyle=':')
+pylab.plot(t, simsol3, color='m', label='Симуляторное при q0 = 0')
+pylab.plot(t, solution3[:, 0], color='k', label='Линейная система при q0 = 0', linestyle=':')
 pylab.legend(loc='best')
-
+'''
 pylab.show()
 
 p.disconnect()
